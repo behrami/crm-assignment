f7dce08b3c4797bad3f6b1e069c4549e7dfb1bde
behrami <behram1993@gmail.com>
2018-03-28 19:26:19 -0400

    completed contact.rb

--- contact.rb
+++ contact.rb
@@ -9,8 +9,7 @@ class Contact
     @last_name = last_name
     @email = email
     @note = note
-
-    @@id = @@next_id
+    @id = @@next_id
     @@next_id+=1
   end
 
@@ -33,38 +32,87 @@ class Contact
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
-
+  def self.find(id)
+    @@contact_list.each do |contact_obj|
+      if contact_obj.id == id
+        return contact_obj
+      end
+   end
+   p "no contact found"
   end
 
   # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
-  def update
+  def update(search_type, replace)
+
+      if search_type == "first_name"
+          self.first_name = replace
+          return self
+
+     elsif search_type == "last_name"
+         contact.last_name = replace
+         return self
+
+    elsif search_type == "email"
+        contact.email = replace
+        return self
+
+    elsif search_type == "note"
+        contact.email = replace
+        return self
+    end
 
   end
 
   # This method should work similarly to the find method above
   # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
+  # by specifying both the name of the sattribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
+  def self.find_by(search_type, search)
+
+    if search_type == "first_name"
+      @@contact_list.each do |contact|
+        if contact.first_name == search
+          return contact
+        end
+     end
+
+   elsif search_type == "last_name"
+     @@contact_list.each do |contact|
+       if contact.last_name == search
+         return contact
+       end
+    end
+
+  elsif search_type == "email"
+    @@contact_list.each do |contact|
+      if contact.email == search
+        return contact
+      end
+   end
+  end
 
   end
 
   # This method should delete all of the contacts
   def self.delete_all
-
+    @@contact_list.clear
   end
 
   def full_name
-
+    return "#{first_name} #{last_name}"
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
+    @@contact_list.each do |contact_obj|
+      if contact_obj.id == self.id
+        @@contact_list.delete(self)
+      end
+    end
 
   end
 
@@ -87,6 +135,10 @@ class Contact
     return @note
   end
 
+  def id
+    return @id
+  end
+
   # WRITERS
   def first_name=(first_name)
     @first_name = first_name
@@ -105,14 +157,23 @@ class Contact
   end
 
 end
-##########################################3
-p contact = Contact.new('Betty', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
-p contact.email # => 'bettymakes@gmail.com'
-p contact.note = 'Loves HTML & CSS'
-p contact
+##########################################testing
+# p contact = Contact.new('Betty', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
+# p contact.email # => 'bettymakes@gmail.com'
+# p contact.note = 'Loves HTML & CSS'
+# p contact
 puts
 
 p test_contact = Contact.create('Bit', 'Tester', 'bittest@gmail.com', 'Loves coding')
 p test_contact2 = Contact.create('Betty', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
 #p Contact.add_phone_book(test_contact)
-p Contact.all
+#p Contact.all
+Contact.find(1)
+Contact.find_by("first_name", "Betty")
+
+p test_contact2.update("first_name", "Behram")
+#p Contact.delete_all
+
+p test_contact.full_name
+
+p test_contact.delete
