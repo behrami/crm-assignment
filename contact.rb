5595c0a2eddb38fd2de6d0551491930bc6f0a7dd
behrami <behram1993@gmail.com>
2018-03-28 15:32:16 -0400

    added methode to push contact to array

--- contact.rb
+++ contact.rb
@@ -1,19 +1,34 @@
 class Contact
 
-  # This method should initialize the contact's attributes
-  def initialize
+  @@contact_list = []
+  @@next_id=1
 
+  # This method should initialize the contact's attributes
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+
+    @@id = @@next_id
+    @@next_id+=1
   end
 
-  # This method should call the initializer, 
+  # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
+  def self.create(first_name, last_name, email, notes)
+    new_contact = Contact.new(first_name, last_name, email, notes)
+    add_phone_book(new_contact)
+    return new_contact
+  end
 
+  def self.add_phone_book(contact)
+    @@contact_list.push(contact)
   end
 
   # This method should return all of the existing contacts
   def self.all
-
+    return @@contact_list
   end
 
   # This method should accept an id as an argument
@@ -22,7 +37,7 @@ class Contact
 
   end
 
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
@@ -54,5 +69,50 @@ class Contact
   end
 
   # Feel free to add other methods here, if you need them.
-  
+
+  # READERS
+  def first_name
+    return @first_name
+  end
+
+  def last_name
+    return @last_name
+  end
+
+  def email
+    return @email
+  end
+
+  def note
+    return @note
+  end
+
+  # WRITERS
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+
+  def last_name=(last_name)
+    @last_name = last_name
+  end
+
+  def email=(email)
+    @email = email
+  end
+
+  def note=(note)
+    @note = note
+  end
+
 end
+##########################################3
+p contact = Contact.new('Betty', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
+p contact.email # => 'bettymakes@gmail.com'
+p contact.note = 'Loves HTML & CSS'
+p contact
+puts
+
+p test_contact = Contact.create('Bit', 'Tester', 'bittest@gmail.com', 'Loves coding')
+p test_contact2 = Contact.create('Betty', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
+#p Contact.add_phone_book(test_contact)
+p Contact.all
